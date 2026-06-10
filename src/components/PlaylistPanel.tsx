6c72b5c8e026b8ca8ff221101c19a085d527188d
gurprit <gurprit.sahota@gmail.com>
2026-06-10 22:51:53 +0100

    Fix playlist panel callback types

--- src/components/PlaylistPanel.tsx
+++ src/components/PlaylistPanel.tsx
@@ -5,6 +5,7 @@ import PanelHeader from './PanelHeader';
 type Track = {
   id: string;
   name: string;
+  uri: string;
 };
 
 type Props = {
@@ -22,7 +23,7 @@ type Props = {
   setSelectedTrackId: (id: string) => void;
   setCurrentTrackName: (name: string) => void;
   addLog: (message: string) => void;
-  autoSyncAndTransfer: (track: Track, playlist: Track[], trackId: string) => void;
+  autoSyncAndTransfer: (track?: Track, playlistSnapshot?: Track[], selectedIdSnapshot?: string | null) => void;
 };
 
 export default function PlaylistPanel({
